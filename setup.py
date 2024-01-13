7574e7e77347bcee67fdeac857f7d255b3e9e1dd
Your Name <you@example.com>
2024-01-13 19:33:20 +0800

    Released v0.0.2

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 setup(
-    name='cc-feishu',  # package name
+    name='cc-onepassword',  # package name
     version=VERSION,  # package version
-    description='my feishu package',  # package description
+    description='my onepassword package',  # package description
     packages=find_packages(),
     url="https://github.com/houm01/cc-onepassword",
     zip_safe=False,
